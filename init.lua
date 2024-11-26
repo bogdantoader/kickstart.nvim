f342bb239837baf5995a1043cc885a161df7cdc3
Bogdan Toader <btoader@mrc-lmb.cam.ac.uk>
2024-11-26 03:45:56 +0000

    code companion

--- init.lua
+++ init.lua
@@ -1001,6 +1001,57 @@ require('lazy').setup {
       alpha.setup(dashboard.config)
     end,
   },
+
+  {
+    'olimorris/codecompanion.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'hrsh7th/nvim-cmp', -- Optional: For using slash commands and variables in the chat buffer
+      'nvim-telescope/telescope.nvim', -- Optional: For using slash commands
+      { 'MeanderingProgrammer/render-markdown.nvim', ft = { 'markdown', 'codecompanion' } }, -- Optional: For prettier markdown rendering
+      { 'stevearc/dressing.nvim', opts = {} }, -- Optional: Improves `vim.ui.select`
+    },
+    -- config = true,
+    config = function()
+      require('codecompanion').setup {
+        display = {
+          diff = {
+            provider = 'mini_diff',
+          },
+        },
+        -- opts = {
+        --   log_level = 'DEBUG',
+        -- },
+        -- strategies = {
+        --   chat = {
+        --     adapter = 'anthropic',
+        --   },
+        --   inline = {
+        --     adapter = 'copilot',
+        --   },
+        -- },
+        -- adapters = {
+        --   anthropic = function()
+        --     return require('codecompanion.adapters').extend('anthropic', {
+        --       env = {
+        --         api_key = 'thekey',
+        --       },
+        --     })
+        --   end,
+        -- },
+      }
+
+      vim.api.nvim_set_keymap('n', '<C-a>', '<cmd>CodeCompanionActions<cr>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('v', '<C-a>', '<cmd>CodeCompanionActions<cr>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<LocalLeader>a', '<cmd>CodeCompanionChat Toggle<cr>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('v', '<LocalLeader>a', '<cmd>CodeCompanionChat Toggle<cr>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('v', 'ga', '<cmd>CodeCompanionChat Add<cr>', { noremap = true, silent = true })
+
+      -- Expand 'cc' into 'CodeCompanion' in the command line
+      vim.cmd [[cab cc CodeCompanion]]
+    end,
+  },
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
